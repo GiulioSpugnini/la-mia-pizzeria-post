e86ea62881fa38d06ab1f2054e79cb7765a2e1e1
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-06-06 16:54:31 +0200

    implemented delete

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -99,5 +99,21 @@ namespace la_mia_pizzeria_static.Controllers
 
             return View("Show", updatePizza);
         }
+        public IActionResult DeletePizza(Pizza pizza)
+        {
+
+            return View("DeletePizza", pizza);
+        }
+        [HttpPost]
+        public IActionResult Delete(Pizza pizza)
+        {
+            Pizza updatePizza = pizze.pizzas.Find(x => x.Id == pizza.Id);
+            if (updatePizza.Id == pizza.Id)
+            {
+                var ok = pizze.pizzas.Remove(updatePizza);
+                Console.WriteLine(ok);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
